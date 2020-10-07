dedeae53b02caa22cac116f72b3397c9a22f2e3e
Jan Buć <jan.buc@aptiv.com>
2020-10-07 13:20:49 +0200

    sep_1

---
+++ sep_ch/7/1.py
@@ -0,0 +1,43 @@
+from typing import List
+
+
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        cache = set()
+
+        seen = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+
+        def check_i_j_in_grid(i, j):
+            if 0 <= i <= len(grid) and 0 <= j <= len(grid[0]):
+                return True
+            else:
+                return False
+
+        def check_if_all_field_marked(grid):
+            for row in grid:
+                if 0 in row:
+                    return False
+
+        def trav(i, j, path):
+            if check_i_j_in_grid(i, j):
+                return
+
+            if grid[i][j] == 1 or grid[i][j] == -1:
+                return
+
+            if grid[i][j] == 2:
+                if check_if_all_field_marked(grid):
+                    cache.add(path)
+                    return
+
+            trav(i - 1, j, path + [(i, j)])
+            trav(i + 1, j, path + [(i, j)])
+            trav(i, j - 1, path + [(i, j)])
+            trav(i, j + 1, path + [(i, j)])
+
+        trav(0, 0, [])
+
+
+print(Solution().uniquePathsIII(grid=[[1, 0, 0], [0, 0, 0], [0, 0, 2]]))
+# print(Solution().uniquePathsIII(grid=[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 2, -1]]))
+# print(Solution().uniquePathsIII(grid=[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 2]]))
